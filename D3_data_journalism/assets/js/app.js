7834f9e30c0f291be0502439ca42e736d05dd065
Kaylon <kcyoung@tiksdd.com>
2020-11-21 16:37:48 -0600

    Finished code for map axes.  Added circles to chart.

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -2,18 +2,6 @@
 // The code for the chart is wrapped inside a function that
 // automatically resizes the chart
 
-// var svgArea = d3.select("#scatter").select("svg");
-
-//     // clear svg is not empty
-//     if (!svgArea.empty()) {
-//     svgArea.remove();
-//     }
-
-
-// var svgWidth = document.getElementById('scatter').clientWidth;
-// var svgHeight = svgWidth / 2.5;
-
-// var svg = d3.select('#scatter').append('svg');
 
 function makeResponsive() {
 
@@ -25,12 +13,9 @@ function makeResponsive() {
     }
     
     svgWidth = document.getElementById('scatter').clientWidth;
-    svgHeight = svgWidth / 2;
+    svgHeight = svgWidth / 1.25;
     
-    // d3.select('#scatter svg')
-    //   .attr('width', svgWidth)
-    //   .attr('height', svgHeight);
-
+    
     // Append SVG element
     var svg = d3
     .select("#scatter")
@@ -63,11 +48,11 @@ function makeResponsive() {
         });
 
         var xPovertyScale = d3.scaleLinear()
-        .domain([0, d3.max(healthData, d => d.poverty)])
+        .domain([d3.min(healthData, d => d.poverty)/1.25, d3.max(healthData, d => d.poverty)*1.05])
         .range([0, chartWidth]);
 
         var yHealthScale = d3.scaleLinear()
-        .domain([0, d3.max(healthData, d => d.healthcare)])
+        .domain([d3.min(healthData, d => d.healthcare)/1.25, d3.max(healthData, d => d.healthcare)*1.05])
         .range([chartHeight, 0]);
 
         // create axes
@@ -82,82 +67,22 @@ function makeResponsive() {
         chartGroup.append("g")
             .call(yAxis);
 
-              
-    
-
-});
-
-
-//     // if the SVG area isn't empty when the browser loads,
-//     // remove it and replace it with a resized version of the chart
-//     var svgArea = d3.select("#scatter").select("svg");
-
-//     // clear svg is not empty
-//     if (!svgArea.empty()) {
-//     svgArea.remove();
-//     }
-
-//     // SVG wrapper dimensions are determined by the current width and
-//     // height of the browser window.
-//     var svgWidth = window.innerWidth ;
-//     var svgHeight = window.innerHeight;
-  
-//     var margin = {
-//         top: 50,
-//         bottom: 50,
-//         right: 50,
-//         left: 50
-//     };
-
-      
-//     var chartHeight = svgHeight - margin.top - margin.bottom;
-//     var chartWidth = svgWidth - margin.left - margin.right;
-
-//     // Append SVG element
-//     var svg = d3
-//     .select("#scatter")
-//     .append("svg")
-//     .attr("height", svgHeight)
-//     .attr("width", svgWidth);
-
-//     // Append group element
-//     var chartGroup = svg.append("g")
-//         .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-//     d3.csv("/assets/data/data.csv").then(function(healthData){
-
-//         healthData.forEach(function(data) {
-           
-//             data.healthcare = +data.healthcare;
-//             data.poverty = +data.poverty;
-
-//             console.log(data)
-//         });
-
-//         var xPovertyScale = d3.scaleLinear()
-//         .domain([0, d3.max(healthData, d => d.poverty)])
-//         .range([0, chartWidth]);
-
-//         var yHealthScale = d3.scaleLinear()
-//         .domain([0, d3.max(healthData, d => d.healthcare)])
-//         .range([chartHeight, 0]);
-
-//         // create axes
-//         var xAxis = d3.axisBottom(xPovertyScale).ticks(6);
-//         var yAxis = d3.axisLeft(yHealthScale).ticks(6);
-
-//         // append axes
-//         chartGroup.append("g")
-//             .attr("transform", `translate(0, ${chartHeight})`)
-//             .call(xAxis);
-
-//         chartGroup.append("g")
-//             .call(yAxis);
+        var circlesGroup = chartGroup.selectAll("circle")
+        .data(healthData)
+        .enter()
+        .append("circle")
+        .attr("class", "stateCircle")
+        .attr("cx", d => xPovertyScale(d.poverty))
+        .attr("cy", d => yHealthScale(d.healthcare))
+        .attr("r", "10");
+        
 
               
     
 
-// });
+    });
+
+
 
 }
 
