d85b15c6577b3e5c52bcd28260b9169f5bcb0349
Kaylon <kcyoung@tiksdd.com>
2020-11-22 17:07:03 -0600

    Updated file path.

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -49,7 +49,7 @@ function makeResponsive() {
     var chartGroup = svg.append("g")
         .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-    d3.csv("D3-Challeng/assets/data/data.csv").then(function(healthData){
+    d3.csv("../assets/data/data.csv").then(function(healthData){
 
         healthData.forEach(function(data) {
            
